bfe86b251c21914462a92de389d8585ce0f7ae6c
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2020-09-27 05:35:36 +0100

    Version 1.

---
+++ index.js
@@ -0,0 +1,112 @@
+const getMoonPhase = (percentage) => {
+    switch(true){
+        case (percentage < 12.5):
+            return 'New Moon'
+        case (percentage < 25):
+            return 'Waxing Crescent'
+        case (percentage < 37.5):
+            return 'First Quarter'
+        case (percentage < 50):
+            return 'Waxing Gibbous'
+        case (percentage < 62.5):
+            return 'Full Moon'
+        case (percentage < 75):
+            return 'Waning Gibbous'
+        case (percentage < 87.5):
+            return 'Third Quarter'
+        case (percentage < 100):
+            return 'Waning Crescent'
+    }
+}
+
+const setDays = (days) => {
+    localStorage.setItem('days', `${days}`)
+}
+
+const getDays = () => {
+    const days = localStorage.getItem('days')
+    
+    if (days === null || days === NaN) {
+        localStorage.setItem('days', '0')
+        return 0;
+    }
+
+    return parseInt(days, 10);
+}
+
+let days = 0;
+let daysIncrement = 1;
+
+const getPhasePercentage = (numberOfDays) => {
+    return ((numberOfDays % 30) / 30) * 100
+}
+
+const setMoonStyle = (days) => {
+    const percentage = getPhasePercentage(days);
+
+    let stylePosition;
+    console.log(percentage);
+
+    if (percentage < 60) {
+       stylePosition = `${percentage / 62.5 * -500}`
+    }
+
+    if (percentage === 60) {
+        stylePosition = `500`
+    }
+    
+    if (percentage > 60) {
+       stylePosition = `${500 - ((percentage - 62.5) /  37.5) * 500}`
+    }
+    document.getElementsByClassName('moon')[0].style.boxShadow = `inset ${stylePosition}px 0px #999, inset ${stylePosition}px 0px 1px 1px #999`
+}
+
+const onInputChange = () => {
+    daysIncrement = dayIncrementInput.value
+}
+
+const setPhaseName = (numberOfDays) => {
+    document.getElementsByClassName('phase-name')[0].innerHTML = `${getMoonPhase(getPhasePercentage(numberOfDays))}`
+}
+
+const setDaysElement = (numberOfDays) => {
+    document.getElementsByClassName('campaign-days')[0].innerHTML = `${numberOfDays}`
+}
+
+const updateMoon = (days) => {
+    setDays(`${days}`)
+    setPhaseName(days)
+    setDaysElement(days);
+    setMoonStyle(days)
+}
+
+const onPositiveButtonPress = () => {
+    days = days + daysIncrement
+    updateMoon(days)
+}
+
+const onNegativeButtonPress = () => {
+    if (days - daysIncrement < 0) {
+        days = 0
+    } else {
+        days = days - daysIncrement
+    }
+    updateMoon(days)
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    days = getDays();
+    daysIncrement = 1;
+
+    document.getElementsByClassName('days-input')[0].value = daysIncrement
+
+    document.getElementsByClassName('days-input')[0].addEventListener('change', () => {
+        daysIncrement = parseInt(document.getElementsByClassName('days-input')[0].value, 10);
+    })
+    
+    setPhaseName(days);
+    setDaysElement(days);
+    setMoonStyle(days)
+});
+
